89ab7d0aa6066ecb6f02faa16ae169d750fc8ac2
jonabasque <jonmultimedia@gmail.com>
2017-01-03 19:30:39 +0100

    Created a simple directive of element type

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-(function()){
+(function(){
+    angular.module("app", ['directives'])
+        .controller('navCtrl', function(){
+            var that = this;
+            this.items = [
+                {
+                    name: 'portada',
+                    route: '/',
+                    order: null
+                },
+                {
+                    name: 'blog',
+                    route: 'blog',
+                    order: null
+                },
+                {
+                    name: 'gallery',
+                    route: 'gallery',
+                    order: null,
 
+                }
+            ];
+            this.select = '/';
+            //this.orientation = 'vertical';
+
+        });
 })();
