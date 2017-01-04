05f13e722fb18133138f57bd99421779159da04b
jonabasque <jonmultimedia@gmail.com>
2017-01-04 18:34:31 +0100

    Created 'myLogo' directive of attr type, without isolated scope & using parent scope attr

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 (function(){
-    angular.module("app", ['directives'])
+    angular.module("app", ['attr.directives', 'elem.directives'])
         .controller('headerCtrl', function(){
             var that = this;
+            this.logo = 'images/logo.jpg';
             //this.orientation = 'vertical';
 
         });
